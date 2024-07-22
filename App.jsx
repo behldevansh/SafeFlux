73331880afab79330f5695bee55a083ba8bac646
Devansh Behl <heydevanshbehl@gmail.com>
2024-07-22 13:05:30 +0530

    initial commit + css setup

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView,ScrollView} from 'react-native';
+import FlatCards from './component/FlatCards';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+
+      </ScrollView>
+        <Text>Hello JSX File</Text>
+        <FlatCards></FlatCards>
+      
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ AppPro.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { View,Text,StyleSheet,useColorScheme} from 'react-native';
+
+function AppPro() : JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+  return(
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Hello World</Text>
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText:{
+    color: 'white',
+  },
+    darkText:{
+        color: 'black',
+    },
+  
+});
+
+export default AppPro;
